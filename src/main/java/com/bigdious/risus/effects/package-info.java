3d0568ad9b42d740f1b5f056436a451121311d4f
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-08-22 20:47:28 -0600

    few more fixes
    - added missing package infos
    - fixed potion of love name to be correct
    - fixed missing bloodweave core textures
    - fixed amnesia icon size
    - fixed AT comment

---
+++ src/main/java/com/bigdious/risus/effects/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.bigdious.risus.effects;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/com/bigdious/risus/inventory/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.bigdious.risus.inventory;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
