99c61db9d5b467d85df9a6c5fe85e733a4b6331e
bigdious <bigdious@gmail.com>
2023-07-19 21:21:08 +0300

    Mating frenzy no work on players now, alteration catalyst textures fixed, mawguts texture fixed

--- src/main/java/com/bigdious/risus/effects/MatingFrenzyEffect.java
+++ src/main/java/com/bigdious/risus/effects/MatingFrenzyEffect.java
@@ -1,11 +1,14 @@
 package com.bigdious.risus.effects;
 
 import com.bigdious.risus.init.RisusMobEffects;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
 
 public class MatingFrenzyEffect extends MobEffect {
 	public MatingFrenzyEffect(MobEffectCategory category, int color) {
@@ -21,6 +24,10 @@ public class MatingFrenzyEffect extends MobEffect {
 				animal.setInLove(null);
 				animal.setAge(0);
 			}
+		}
+		if (entity instanceof Player player) {
+			entity.removeEffect(RisusMobEffects.MATING_FRENZY.get());
+			player.sendSystemMessage(Component.literal(ChatFormatting.DARK_RED + "You are incapable of Love."));
 		}
 	}
 
