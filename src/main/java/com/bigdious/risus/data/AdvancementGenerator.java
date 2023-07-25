e385e5ca8c347ef6482802c4233bed7092dca834
bigdious <bigdious@gmail.com>
2023-07-25 16:49:40 +0300

    AdvancementGenerator Prototype

---
+++ src/main/java/com/bigdious/risus/data/AdvancementGenerator.java
@@ -0,0 +1,48 @@
+package com.bigdious.risus.data;
+
+
+import com.bigdious.risus.Risus;
+import com.bigdious.risus.init.RisusBlocks;
+import com.bigdious.risus.init.RisusItems;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.FrameType;
+import net.minecraft.advancements.RequirementsStrategy;
+import net.minecraft.advancements.critereon.*;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.advancements.AdvancementProvider;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import java.util.function.Consumer;
+
+public class AdvancementGenerator extends AdvancementProvider {
+    public AdvancementGenerator(DataGenerator generator, ExistingFileHelper helper) {
+        super(generator, helper);
+    }
+    @Override
+    protected void registerAdvancements(Consumer<Advancement> consumer, ExistingFileHelper helper) {
+
+        Advancement first = Advancement.Builder.advancement().display(
+                        RisusItems.SMILE.get(),
+                        Component.translatable("itemGroup.risus"),
+                        Component.translatable("advancement.risus.first.desc"),
+                        new ResourceLocation(Risus.MODID ,"textures/block/ashen_remains.png"),
+                        FrameType.TASK,
+                        true, false, false)
+                .addCriterion("organic", InventoryChangeTrigger.TriggerInstance.hasItems(RisusItems.ORGANIC_MATTER.get()))
+                .save(consumer, "risus:first");
+
+        Advancement fleshing = Advancement.Builder.advancement().parent(first).display(
+                RisusBlocks.DECOMPOSED_TISSUE.get(),
+                    Component.translatable("advancement.risus.fleshing"),
+                    Component.translatable("advancement.risus.fleshing.desc"), null, FrameType.TASK, true,true,false)
+                .requirements(RequirementsStrategy.OR)
+                .addCriterion("fleshing", ItemInteractWithBlockTrigger.TriggerInstance.itemUsedOnBlock(LocationPredicate.Builder.location().setBlock(BlockPredicate.Builder.block().of(RisusBlocks.DECOMPOSING_TISSUE.get()).build()),ItemPredicate.Builder.item().of(RisusItems.ORGANIC_MATTER.get())))
+                .addCriterion("fleshing", ItemInteractWithBlockTrigger.TriggerInstance.itemUsedOnBlock(LocationPredicate.Builder.location().setBlock(BlockPredicate.Builder.block().of(RisusBlocks.ROTTING_TISSUE.get()).build()),ItemPredicate.Builder.item().of(RisusItems.ORGANIC_MATTER.get())))
+                .addCriterion("fleshing", ItemInteractWithBlockTrigger.TriggerInstance.itemUsedOnBlock(LocationPredicate.Builder.location().setBlock(BlockPredicate.Builder.block().of(RisusBlocks.DECAYING_TISSUE.get()).build()),ItemPredicate.Builder.item().of(RisusItems.ORGANIC_MATTER.get())))
+                .addCriterion("fleshing", ItemInteractWithBlockTrigger.TriggerInstance.itemUsedOnBlock(LocationPredicate.Builder.location().setBlock(BlockPredicate.Builder.block().of(RisusBlocks.TISSUE.get()).build()),ItemPredicate.Builder.item().of(RisusItems.ORGANIC_MATTER.get())))
+                .save(consumer, "risus:fleshing");
+
+    }
+}
