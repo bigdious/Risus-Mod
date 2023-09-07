10f6c01bc1c64c3823f3ed161b2e15d663c413e9
bigdious <bigdious@gmail.com>
2023-09-07 15:57:58 +0300

    Angel goal prototype

--- src/main/java/com/bigdious/risus/entity/Angel.java
+++ src/main/java/com/bigdious/risus/entity/Angel.java
@@ -1,14 +1,19 @@
 package com.bigdious.risus.entity;
 
 import com.bigdious.risus.init.RisusItems;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -17,7 +22,7 @@ public class Angel extends Monster {
 	public Angel(EntityType<? extends Monster> type, Level level) {
 		super(type, level);
 	}
-
+	private static final EntityDataAccessor<Boolean> DATA_IS_CHARGING = SynchedEntityData.defineId(Angel.class, EntityDataSerializers.BOOLEAN);
 	public static AttributeSupplier.Builder attributes() {
 		return Monster.createMonsterAttributes()
 				.add(Attributes.MAX_HEALTH, 1024.0D)
@@ -25,14 +30,67 @@ public class Angel extends Monster {
 				.add(Attributes.ATTACK_DAMAGE, 5.0D);
 	}
 
+	public void setCharging(boolean p_32759_) {
+		this.entityData.set(DATA_IS_CHARGING, p_32759_);
+	}
+	public boolean isCharging() {
+		return this.entityData.get(DATA_IS_CHARGING);
+	}
+
 	@Override
 	protected void registerGoals() {
 		super.registerGoals();
-		this.goalSelector.addGoal(0, new FloatGoal(this));
-		this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 50.0F));
+		this.goalSelector.addGoal(5, new FloatGoal(this));
+		this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 50.0F));
+		this.goalSelector.addGoal(7, new Angel.AngelLightningAttackGoal(this));
+		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, (entity) -> Math.abs(entity.getY() - this.getY()) <= 4.0D));
 
 	}
+	static class AngelLightningAttackGoal extends Goal {
+		private final Angel angel;
+		public int chargeTime;
 
+		public AngelLightningAttackGoal(Angel angel) {
+			this.angel = angel;
+		}
+
+		public boolean canUse() {
+			return this.angel.getTarget() != null;
+		}
+
+		public void start() {
+			this.chargeTime = 0;
+		}
+
+		public void stop() {
+			this.angel.setCharging(false);
+		}
+
+		public boolean requiresUpdateEveryTick() {
+			return true;
+		}
+
+		public void tick() {
+			LivingEntity livingentity = this.angel.getTarget();
+			if (livingentity != null) {
+				if (livingentity.distanceToSqr(this.angel) < 4096.0D && this.angel.hasLineOfSight(livingentity)) {
+					Level level = this.angel.level;
+					++this.chargeTime;
+
+					if (this.chargeTime == 20) {
+						LightningBolt lightning = new LightningBolt(EntityType.LIGHTNING_BOLT, level );
+						lightning.setPos(livingentity.getX(), livingentity.getY(), livingentity.getY());
+						level.addFreshEntity(lightning);
+						this.chargeTime = -40;
+					}
+				} else if (this.chargeTime > 0) {
+					--this.chargeTime;
+				}
+
+				this.angel.setCharging(this.chargeTime > 10);
+			}
+		}
+	}
 	@Override
 	public boolean hurt(DamageSource source, float amount) {
 		if (source.getEntity() instanceof LivingEntity living && living.getItemInHand(living.getUsedItemHand()).is(RisusItems.UNAWAKENED_VESSEL.get())) {
