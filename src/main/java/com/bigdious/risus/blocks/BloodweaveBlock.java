2c2ceb10c7f4c6ac9276999642c4fc2bc33c87b4
bigdious <bigdious@gmail.com>
2023-11-28 21:55:16 +0200

    furtherbloodweaveupdates

--- src/main/java/com/bigdious/risus/blocks/BloodweaveBlock.java
+++ src/main/java/com/bigdious/risus/blocks/BloodweaveBlock.java
@@ -11,13 +11,16 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.PipeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import org.jetbrains.annotations.Nullable;
 
 //oh god, this will be a 6 dimensional fence
 //nvm it's just chorus fruit
 
 public class BloodweaveBlock extends PipeBlock {
+	public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
 	public BloodweaveBlock(Properties properties) {
 		super(0.3125F, properties);
 		this.registerDefaultState(this.stateDefinition.any()
@@ -26,16 +29,22 @@ public class BloodweaveBlock extends PipeBlock {
 			.setValue(SOUTH, Boolean.valueOf(false))
 			.setValue(WEST, Boolean.valueOf(false)).setValue(UP, Boolean.valueOf(false))
 			.setValue(DOWN, Boolean.valueOf(false)));
+		this.registerDefaultState(this.defaultBlockState().setValue(AXIS, Direction.Axis.Y));
 	}
+
 
 	public BlockState updateShape(BlockState state, Direction dir, BlockState otherstate, LevelAccessor levelacc, BlockPos pos, BlockPos pos2) {
 		boolean flag = otherstate.is(this);
 		return state.setValue(PROPERTY_BY_DIRECTION.get(dir), Boolean.valueOf(flag));
 
 	}
+	public BlockState getStateForPlacement(BlockPlaceContext p_55928_) {
+		return this.defaultBlockState().setValue(AXIS, p_55928_.getClickedFace().getAxis());
+	}
+
 
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_51735_) {
-		p_51735_.add(NORTH, EAST, SOUTH, WEST, UP, DOWN);
+		p_51735_.add(NORTH, EAST, SOUTH, WEST, UP, DOWN,AXIS);
 	}
 }
 
--- src/main/java/com/bigdious/risus/init/RisusSoundEvents.java
+++ src/main/java/com/bigdious/risus/init/RisusSoundEvents.java
@@ -9,7 +9,11 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class RisusSoundEvents {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Risus.MODID);
-
+	public static final RegistryObject<SoundEvent> MEAT_BREAK = register("block.meat.break");
+	public static final RegistryObject<SoundEvent> MEAT_FALL = register("block.meat.fall");
+	public static final RegistryObject<SoundEvent> MEAT_HIT = register("block.meat.hit");
+	public static final RegistryObject<SoundEvent> MEAT_PLACE = register("block.meat.place");
+	public static final RegistryObject<SoundEvent> MEAT_STEP = register("block.meat.step");
     private static RegistryObject<SoundEvent> register(String name) {
         return SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Risus.MODID, name)));
     }
