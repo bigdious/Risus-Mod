c718294c42b5c4394953d86883ceb66f2106698f
bigdious <bigdious@gmail.com>
2024-04-30 19:22:22 +0300

    Boomstick tweaks

--- src/main/java/com/bigdious/risus/items/BoomstickItem.java
+++ src/main/java/com/bigdious/risus/items/BoomstickItem.java
@@ -2,11 +2,14 @@ package com.bigdious.risus.items;
 
 import com.bigdious.risus.init.RisusItems;
 import com.bigdious.risus.init.RisusMobEffects;
+import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -15,8 +18,11 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.common.Tags;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -47,21 +53,28 @@ public class BoomstickItem extends Item {
 		});
 		return flag.get();
 	}
-	//TODO make boomstick work when enemies hold it
+	//TODO make it work with enemies
 	@Override
 	public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
 		Level level = pAttacker.level();
 		ItemStack itemstack = pAttacker.getMainHandItem();
-		if (pAttacker.isHolding(RisusItems.BOOMSTICK.get())) {
-			PrimedTnt primedtnt = new PrimedTnt(level, pTarget.getX() + 0.5, pTarget.getY(), pTarget.getZ() + 0.5, pAttacker);
-			level.addFreshEntity(primedtnt);
-			primedtnt.setFuse(0);
+		if (pTarget.getType() != EntityType.ARMOR_STAND
+			&& pTarget.getType() != EntityType.BOAT
+			&& pTarget.getType() != EntityType.CHEST_BOAT
+			&& pTarget.getType() != EntityType.ITEM_FRAME
+			&& pTarget.getType() != EntityType.GLOW_ITEM_FRAME
+			&& pTarget.getType() != EntityType.MINECART)
+		if (!level.isClientSide()) {
+			this.explode(level, pTarget.getOnPos(), pAttacker);
 			itemstack.hurt(1, level.getRandom(), null);
-			level.playSound(null, primedtnt.getX(), primedtnt.getY(), primedtnt.getZ(), SoundEvents.TNT_PRIMED, SoundSource.BLOCKS, 1.0F, 1.0F);
+
 			}
 		return super.hurtEnemy(pStack, pTarget, pAttacker);
 	}
-
+	private void explode(Level level, BlockPos pos, LivingEntity entity) {
+		Vec3 vec3 = pos.getCenter().add(0, 1, 0);
+		level.explode(null, level.damageSources().explosion(entity, entity), null, vec3, 3F, false, Level.ExplosionInteraction.BLOCK);
+	}
 
 	@Override
 	public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
