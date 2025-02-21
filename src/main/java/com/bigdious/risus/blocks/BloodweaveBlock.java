9ce9bc13a15e6465b8d52eb5cca3db3f77deb7ad
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2025-02-21 15:36:28 -0700

    fix bloodweave generation

--- src/main/java/com/bigdious/risus/blocks/BloodweaveBlock.java
+++ src/main/java/com/bigdious/risus/blocks/BloodweaveBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.PipeBlock;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
@@ -30,14 +31,14 @@ public class BloodweaveBlock extends PipeBlock implements SimpleMultiloggedBlock
 	public BloodweaveBlock(Properties properties) {
 		super(0.3125F, properties);
 		this.registerDefaultState(this.stateDefinition.any()
-				.setValue(NORTH, Boolean.FALSE)
-				.setValue(EAST, Boolean.FALSE)
-				.setValue(SOUTH, Boolean.FALSE)
-				.setValue(WEST, Boolean.FALSE)
-				.setValue(UP, Boolean.FALSE)
-				.setValue(DOWN, Boolean.FALSE)
-				.setValue(AXIS, Direction.Axis.Y)
-				.setValue(FLUIDLOGGED, MultiloggingEnum.EMPTY));
+			.setValue(NORTH, false)
+			.setValue(EAST, false)
+			.setValue(SOUTH, false)
+			.setValue(WEST, false)
+			.setValue(UP, false)
+			.setValue(DOWN, false)
+			.setValue(AXIS, Direction.Axis.Y)
+			.setValue(FLUIDLOGGED, MultiloggingEnum.EMPTY));
 	}
 
 	@Override
@@ -72,31 +73,48 @@ public class BloodweaveBlock extends PipeBlock implements SimpleMultiloggedBlock
 
 	}
 
+	@Override
 	public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
 		entity.makeStuckInBlock(state, new Vec3(0.65D, 0.75F, 0.65D));
 	}
+
+	@Override
 	protected BlockState rotate(BlockState state, Rotation rot) {
-		return rotatePillar(state, rot);
+		return rotatePillar(state, rot)
+			.setValue(PROPERTY_BY_DIRECTION.get(rot.rotate(Direction.NORTH)), state.getValue(NORTH))
+			.setValue(PROPERTY_BY_DIRECTION.get(rot.rotate(Direction.SOUTH)), state.getValue(SOUTH))
+			.setValue(PROPERTY_BY_DIRECTION.get(rot.rotate(Direction.EAST)), state.getValue(EAST))
+			.setValue(PROPERTY_BY_DIRECTION.get(rot.rotate(Direction.WEST)), state.getValue(WEST))
+			.setValue(PROPERTY_BY_DIRECTION.get(rot.rotate(Direction.UP)), state.getValue(UP))
+			.setValue(PROPERTY_BY_DIRECTION.get(rot.rotate(Direction.DOWN)), state.getValue(DOWN));
 	}
 
 	public static BlockState rotatePillar(BlockState state, Rotation rotation) {
-		switch (rotation) {
-			case COUNTERCLOCKWISE_90:
-			case CLOCKWISE_90:
-				return switch (state.getValue(AXIS)) {
-					case X -> state.setValue(AXIS, Direction.Axis.Z);
-					case Z -> state.setValue(AXIS, Direction.Axis.X);
-					default -> state;
-				};
-			default:
-				return state;
-		}
+		return switch (rotation) {
+			case COUNTERCLOCKWISE_90, CLOCKWISE_90 -> switch (state.getValue(AXIS)) {
+				case X -> state.setValue(AXIS, Direction.Axis.Z);
+				case Z -> state.setValue(AXIS, Direction.Axis.X);
+				default -> state;
+			};
+			default -> state;
+		};
+	}
+
+	@Override
+	protected BlockState mirror(BlockState state, Mirror mirror) {
+		return state.setValue(PROPERTY_BY_DIRECTION.get(mirror.mirror(Direction.NORTH)), state.getValue(NORTH))
+			.setValue(PROPERTY_BY_DIRECTION.get(mirror.mirror(Direction.SOUTH)), state.getValue(SOUTH))
+			.setValue(PROPERTY_BY_DIRECTION.get(mirror.mirror(Direction.EAST)), state.getValue(EAST))
+			.setValue(PROPERTY_BY_DIRECTION.get(mirror.mirror(Direction.WEST)), state.getValue(WEST))
+			.setValue(PROPERTY_BY_DIRECTION.get(mirror.mirror(Direction.UP)), state.getValue(UP))
+			.setValue(PROPERTY_BY_DIRECTION.get(mirror.mirror(Direction.DOWN)), state.getValue(DOWN));
 	}
 
 	@Override
 	public boolean isStickyBlock(BlockState state) {
 		return true;
 	}
+
 	//racist slime block
 	@Override
 	public boolean canStickTo(BlockState state, BlockState other) {
