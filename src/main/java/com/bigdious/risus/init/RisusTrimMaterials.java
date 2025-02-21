eab3276fd0b61facf5c8ea5aa22d25812d3bb3bd
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2025-02-21 15:36:49 -0700

    fix trim item colors

--- src/main/java/com/bigdious/risus/init/RisusTrimMaterials.java
+++ src/main/java/com/bigdious/risus/init/RisusTrimMaterials.java
@@ -15,15 +15,19 @@ import net.minecraft.world.item.armortrim.TrimMaterial;
 import java.util.Map;
 
 public class RisusTrimMaterials {
+
 	public static final ResourceKey<TrimMaterial> GLUTTONY_SCALES = registerKey("gluttony_scales");
 	public static final ResourceKey<TrimMaterial> SKIN = registerKey("skin");
+
 	private static ResourceKey<TrimMaterial> registerKey(String name) {
 		return ResourceKey.create(Registries.TRIM_MATERIAL, ResourceLocation.fromNamespaceAndPath(Risus.MODID, name));
 	}
+
 	public static void bootstrap(BootstrapContext<TrimMaterial> context) {
-		register(context, GLUTTONY_SCALES, RisusItems.GLUTTONY_SCALES, Style.EMPTY.withColor(0x660000), 0.2F);
-		register(context, SKIN, RisusBlocks.SKIN.asItem().builtInRegistryHolder(), Style.EMPTY.withColor(0xf2a688), 0.2F);
+		register(context, GLUTTONY_SCALES, RisusItems.GLUTTONY_SCALES, Style.EMPTY.withColor(0x660000), 0.3F);
+		register(context, SKIN, RisusBlocks.SKIN.asItem().builtInRegistryHolder(), Style.EMPTY.withColor(0xf2a688), 0.5F);
 	}
+
 	private static void register(BootstrapContext<TrimMaterial> context, ResourceKey<TrimMaterial> trimKey, Holder<Item> trimItem, Style color, float itemModelIndex) {
 		TrimMaterial material = new TrimMaterial(trimKey.location().getPath(), trimItem, itemModelIndex, Map.of(), Component.translatable(Util.makeDescriptionId("trim_material", trimKey.location())).withStyle(color));
 		context.register(trimKey, material);
