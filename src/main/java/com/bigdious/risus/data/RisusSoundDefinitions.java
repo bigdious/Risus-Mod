cf4e8c9e4eb5aef54759adffa80883dad5c17c62
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2025-02-21 23:57:57 -0700

    begin custom subtitle work

--- src/main/java/com/bigdious/risus/data/RisusSoundDefinitions.java
+++ src/main/java/com/bigdious/risus/data/RisusSoundDefinitions.java
@@ -3,13 +3,20 @@ package com.bigdious.risus.data;
 import com.bigdious.risus.Risus;
 import com.bigdious.risus.init.RisusSoundEvents;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.common.data.SoundDefinition;
 import net.neoforged.neoforge.common.data.SoundDefinitionsProvider;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import org.jetbrains.annotations.Nullable;
 
 public class RisusSoundDefinitions extends SoundDefinitionsProvider {
 	public RisusSoundDefinitions(PackOutput output, ExistingFileHelper helper) {
 		super(output, Risus.MODID, helper);
 	}
+
 	@Override
 	public void registerSounds() {
 		// vanilla shit
@@ -17,26 +24,95 @@ public class RisusSoundDefinitions extends SoundDefinitionsProvider {
 //			sound("minecraft:entity.goat.screaming.long_jump").stream()
 //		));
 
-		this.add(RisusSoundEvents.MUSIC_DISC_RAK, definition().with(
-			sound("risus:music/disc/rak").volume(2).pitch(1).weight(1).stream()
-		));
-		this.add(RisusSoundEvents.MUSIC_DISC_MORK, definition().with(
-			sound("risus:music/disc/mork").volume(2).pitch(1).weight(1).stream()
-		));
-		this.add(RisusSoundEvents.MUSIC_DISC_FEIGR, definition().with(
-			sound("risus:music/disc/feigr").volume(2).pitch(1).weight(1).stream()
-		));
-		this.add(RisusSoundEvents.MUSIC_DISC_REGN, definition().with(
-			sound("risus:music/disc/regn").volume(2).pitch(1).weight(1).stream()
-		));
-		this.add(RisusSoundEvents.AMBIENT_MORK, definition().with(
-			sound("risus:music/ambient/ambient_mork").volume(1).pitch(1).weight(1).stream()
-		));
-		this.add(RisusSoundEvents.AMBIENT_FEIGR, definition().with(
-			sound("risus:music/ambient/ambient_feigr").volume(1).pitch(1).weight(1).stream()
-		));
-		this.add(RisusSoundEvents.CHEEKY_LAUGH, definition().with(
-			sound("risus:mobs/cheeky_laugh").volume(1).pitch(1).weight(1).stream()
-		));
+		this.add(RisusSoundEvents.MUSIC_DISC_RAK, definition().with(sound("risus:music/disc/rak").volume(2).stream()));
+		this.add(RisusSoundEvents.MUSIC_DISC_MORK, definition().with(sound("risus:music/disc/mork").volume(2).stream()));
+		this.add(RisusSoundEvents.MUSIC_DISC_FEIGR, definition().with(sound("risus:music/disc/feigr").volume(2).stream()));
+		this.add(RisusSoundEvents.MUSIC_DISC_REGN, definition().with(sound("risus:music/disc/regn").volume(2).stream()));
+		this.add(RisusSoundEvents.AMBIENT_MORK, definition().with(sound("risus:music/ambient/ambient_mork").stream()));
+		this.add(RisusSoundEvents.AMBIENT_FEIGR, definition().with(sound("risus:music/ambient/ambient_feigr").stream()));
+
+		this.generateNewSoundWithSubtitle(RisusSoundEvents.CHEEKY_LAUGH, "entity/holder/cheeky_laugh", 1, "Holder laughs");
+
+		this.generateExistingSoundWithSubtitle(RisusSoundEvents.ARMOR_EQUIP_SKIN, SoundEvents.ARMOR_EQUIP_LEATHER.value(), "Skin armor rustles");
+		this.generateExistingSoundWithSubtitle(RisusSoundEvents.ARMOR_EQUIP_WINGS, SoundEvents.ARMOR_EQUIP_ELYTRA.value(), "Angel Wings rustle");
+	}
+
+	public void generateNewSoundWithSubtitle(DeferredHolder<SoundEvent, SoundEvent> event, String baseSoundDirectory, int numberOfSounds, String subtitle) {
+		generateNewSound(event, baseSoundDirectory, numberOfSounds, subtitle);
+	}
+
+	public void generateNewSound(DeferredHolder<SoundEvent, SoundEvent> event, String baseSoundDirectory, int numberOfSounds, @Nullable String subtitle) {
+		SoundDefinition definition = SoundDefinition.definition();
+		if (subtitle != null) {
+			this.createSubtitleAndLangEntry(event, definition, subtitle);
+		}
+		for (int i = 1; i <= numberOfSounds; i++) {
+			definition.with(SoundDefinition.Sound.sound(Risus.prefix(baseSoundDirectory + (numberOfSounds > 1 ? i : "")), SoundDefinition.SoundType.SOUND));
+		}
+		this.add(event, definition);
+	}
+
+	public void generateNewSoundMC(DeferredHolder<SoundEvent, SoundEvent> event, String baseSoundDirectory, int numberOfSounds, @Nullable String subtitle) {
+		SoundDefinition definition = SoundDefinition.definition();
+		if (subtitle != null) {
+			this.createSubtitleAndLangEntry(event, definition, subtitle);
+		}
+		for (int i = 1; i <= numberOfSounds; i++) {
+			definition.with(SoundDefinition.Sound.sound(ResourceLocation.withDefaultNamespace(baseSoundDirectory + (numberOfSounds > 1 ? i : "")), SoundDefinition.SoundType.SOUND));
+		}
+		this.add(event, definition);
+	}
+
+	public void generateExistingSoundWithSubtitle(DeferredHolder<SoundEvent, SoundEvent> event, SoundEvent referencedSound, String subtitle) {
+		this.generateExistingSoundWithSubtitle(event, referencedSound, subtitle, 1.0F, 1.0F);
+	}
+
+	public void generateExistingSoundWithSubtitle(DeferredHolder<SoundEvent, SoundEvent> event, SoundEvent referencedSound, String subtitle, float volume, float pitch) {
+		this.generateExistingSound(event, referencedSound, subtitle, volume, pitch);
+	}
+
+	public void generateSoundWithExistingSubtitle(DeferredHolder<SoundEvent, SoundEvent> event, SoundEvent referencedSound, String subtitle) {
+		this.add(event, SoundDefinition.definition()
+			.subtitle(subtitle)
+			.with(SoundDefinition.Sound.sound(referencedSound.getLocation(), SoundDefinition.SoundType.EVENT)));
+	}
+
+	public void generateExistingSound(DeferredHolder<SoundEvent, SoundEvent> event, SoundEvent referencedSound, @Nullable String subtitle, float volume, float pitch) {
+		SoundDefinition definition = SoundDefinition.definition();
+		if (subtitle != null) {
+			this.createSubtitleAndLangEntry(event, definition, subtitle);
+		}
+		this.add(event, definition
+			.with(SoundDefinition.Sound.sound(referencedSound.getLocation(), SoundDefinition.SoundType.EVENT).volume(volume).pitch(pitch)));
+	}
+
+	public void makeStepSound(DeferredHolder<SoundEvent, SoundEvent> event, SoundEvent referencedSound) {
+		this.add(event, SoundDefinition.definition()
+			.subtitle("subtitles.block.generic.footsteps")
+			.with(SoundDefinition.Sound.sound(referencedSound.getLocation(), SoundDefinition.SoundType.EVENT)));
+	}
+
+	public void makeNewStepSound(DeferredHolder<SoundEvent, SoundEvent> event, String baseSoundDirectory, int numberOfSounds) {
+		SoundDefinition definition = SoundDefinition.definition();
+		for (int i = 1; i <= numberOfSounds; i++) {
+			definition.with(SoundDefinition.Sound.sound(Risus.prefix(baseSoundDirectory + (numberOfSounds > 1 ? i : "")), SoundDefinition.SoundType.SOUND));
+		}
+		this.add(event, definition.subtitle("subtitles.block.generic.footsteps"));
+	}
+
+	public void makeNewGenericSound(DeferredHolder<SoundEvent, SoundEvent> event, String baseSoundDirectory, int numberOfSounds, @Nullable String type) {
+		SoundDefinition definition = SoundDefinition.definition();
+		for (int i = 1; i <= numberOfSounds; i++) {
+			definition.with(SoundDefinition.Sound.sound(Risus.prefix(baseSoundDirectory + (numberOfSounds > 1 ? i : "")), SoundDefinition.SoundType.SOUND));
+		}
+		this.add(event, type != null ? definition.subtitle("subtitles.block.generic." + type) : definition);
+	}
+
+	private void createSubtitleAndLangEntry(DeferredHolder<SoundEvent, SoundEvent> event, SoundDefinition definition, String subtitle) {
+		String[] splitSoundName = event.getId().getPath().split("\\.", 3);
+		String subtitleKey = "subtitles.risus." + splitSoundName[0] + "." + splitSoundName[2];
+		definition.subtitle(subtitleKey);
+		//TODO Gizmo: hook up subtitle generation to lang file
+		//LangGenerator.SUBTITLE_GENERATOR.put(subtitleKey, subtitle);
 	}
 }
--- src/main/java/com/bigdious/risus/init/RisusSoundEvents.java
+++ src/main/java/com/bigdious/risus/init/RisusSoundEvents.java
@@ -20,7 +20,38 @@ public class RisusSoundEvents {
 	public static final DeferredHolder<SoundEvent, SoundEvent> MUSIC_DISC_REGN = register("music.disc.regn");
 	public static final DeferredHolder<SoundEvent, SoundEvent> AMBIENT_MORK = register("music.ambient.mork");
 	public static final DeferredHolder<SoundEvent, SoundEvent> AMBIENT_FEIGR = register("music.ambient.feigr");
-	public static final DeferredHolder<SoundEvent, SoundEvent> CHEEKY_LAUGH = register("mobs.cheeky_laugh");
+	public static final DeferredHolder<SoundEvent, SoundEvent> CHEEKY_LAUGH = register("entity.holder.cheeky_laugh");
+
+	public static final DeferredHolder<SoundEvent, SoundEvent> BLOOD_AMBIENT = register("block.blood.ambient");
+	public static final DeferredHolder<SoundEvent, SoundEvent> CRYSTALLIZED_BOND_GROW = register("block.popping_bondknot.crystallized_bond_grow");
+	public static final DeferredHolder<SoundEvent, SoundEvent> DEPTH_VASE_INSERT = register("block.depth_vase.insert");
+	public static final DeferredHolder<SoundEvent, SoundEvent> DEPTH_VASE_INSERT_FAIL = register("block.depth_vase.insert_fail");
+	public static final DeferredHolder<SoundEvent, SoundEvent> HAIR_GROW = register("block.hair.grow");
+	public static final DeferredHolder<SoundEvent, SoundEvent> ZIT_POP = register("block.zit.pop");
+
+	public static final DeferredHolder<SoundEvent, SoundEvent> LITTER_LAY_EXCREMENT = register("entity.litter.excrement");
+	public static final DeferredHolder<SoundEvent, SoundEvent> LOVER_INFECT = register("entity.lover.infect");
+	public static final DeferredHolder<SoundEvent, SoundEvent> MAW_GUTS_SHATTER = register("entity.maw.guts_shatter");
+	public static final DeferredHolder<SoundEvent, SoundEvent> SINGER_SCREAM = register("entity.singer.scream");
+	public static final DeferredHolder<SoundEvent, SoundEvent> CRESCENT_DISASTER_HIT = register("entity.crescent_disaster.hit");
+	public static final DeferredHolder<SoundEvent, SoundEvent> CRESCENT_DISASTER_HIT_GROUND = register("entity.crescent_disaster.hit_ground");
+	public static final DeferredHolder<SoundEvent, SoundEvent> CRESCENT_DISASTER_RETURN = register("entity.crescent_disaster.return");
+	public static final DeferredHolder<SoundEvent, SoundEvent> CRESCENT_DISASTER_THROW = register("entity.crescent_disaster.throw");
+	public static final DeferredHolder<SoundEvent, SoundEvent> BLOOD_SLASH_WHOOSH = register("entity.blood_slash.whoosh");
+
+	public static final DeferredHolder<SoundEvent, SoundEvent> EGG_SAC_BREAK = register("item.egg_sac.break");
+	public static final DeferredHolder<SoundEvent, SoundEvent> EGG_SAC_THROW = register("item.egg_sac.throw");
+	public static final DeferredHolder<SoundEvent, SoundEvent> UNYIELDING_USE = register("item.totem_of_unyielding.use");
+	public static final DeferredHolder<SoundEvent, SoundEvent> ARMOR_EQUIP_SKIN = register("item.armor.equip_skin");
+	public static final DeferredHolder<SoundEvent, SoundEvent> ARMOR_EQUIP_WINGS = register("item.armor.equip_wings");
+	public static final DeferredHolder<SoundEvent, SoundEvent> CONCENTRATION_CORE_LITTER = register("item.concentration_core.create_litter");
+	public static final DeferredHolder<SoundEvent, SoundEvent> STRIPPER_STRIP = register("item.stripper.strip");
+	public static final DeferredHolder<SoundEvent, SoundEvent> DARKNESS_PLACE = register("item.light_devourer.place_darkness");
+	public static final DeferredHolder<SoundEvent, SoundEvent> ORGANIC_MATTER_USE = register("item.organic_matter.use");
+	public static final DeferredHolder<SoundEvent, SoundEvent> THOUSAND_BLADE_SLASH = register("item.boat.slash");
+	public static final DeferredHolder<SoundEvent, SoundEvent> TOOTHKNOCKER_CRACK = register("item.toothknocker.crack");
+	public static final DeferredHolder<SoundEvent, SoundEvent> TOOTHKNOCKER_DASH = register("item.toothknocker.dash");
+
 
 	private static DeferredHolder<SoundEvent, SoundEvent> register(String name) {
 		return SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(Risus.prefix(name)));
