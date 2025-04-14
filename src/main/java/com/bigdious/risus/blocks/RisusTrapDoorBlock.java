9871e95ab529ab1c99d77071371320fc2e9a12c0
bigdious <bigdious@gmail.com>
2025-04-14 22:04:10 +0300

    fixed trapdoor crash

--- src/main/java/com/bigdious/risus/blocks/RisusTrapDoorBlock.java
+++ src/main/java/com/bigdious/risus/blocks/RisusTrapDoorBlock.java
@@ -1,13 +1,17 @@
 package com.bigdious.risus.blocks;
 
 import com.bigdious.risus.blocks.interfaces.SimpleMultiloggedBlock;
+import com.bigdious.risus.init.RisusFluids;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.TrapDoorBlock;
@@ -18,16 +22,21 @@ import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.Half;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
 
 import javax.annotation.Nullable;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 
 public class RisusTrapDoorBlock extends TrapDoorBlock implements SimpleMultiloggedBlock {
 
 	public static final EnumProperty<MultiloggingEnum> FLUIDLOGGED = MultiloggingEnum.FLUIDLOGGED;
+	private final BlockSetType type;
 
 	public RisusTrapDoorBlock(BlockSetType type, Properties properties) {
 		super(type, properties);
+		this.type = type;
 		StateDefinition.Builder<Block, BlockState> builder = new StateDefinition.Builder<>(this);
 		builder.add(FACING, OPEN, HALF, POWERED, FLUIDLOGGED);
 		this.stateDefinition = builder.create(Block::defaultBlockState, BlockState::new);
@@ -37,6 +46,38 @@ public class RisusTrapDoorBlock extends TrapDoorBlock implements SimpleMultilogg
 				.setValue(HALF, Half.BOTTOM)
 				.setValue(POWERED, false)
 				.setValue(FLUIDLOGGED, MultiloggingEnum.EMPTY));
+	}
+	@Override
+	protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+		if (!this.type.canOpenByHand()) {
+			return InteractionResult.PASS;
+		} else {
+			this.toggle(state, level, pos, player);
+			return InteractionResult.sidedSuccess(level.isClientSide);
+		}
+	}
+
+	@Override
+	protected void onExplosionHit(BlockState state, Level level, BlockPos pos, Explosion explosion, BiConsumer<ItemStack, BlockPos> dropConsumer) {
+		if (explosion.canTriggerBlocks() && this.type.canOpenByWindCharge() && !state.getValue(POWERED)) {
+			this.toggle(state, level, pos,
+				null);
+		}
+
+		super.onExplosionHit(state, level, pos, explosion, dropConsumer);
+	}
+
+	private void toggle(BlockState state, Level level, BlockPos pos, @Nullable Player player) {
+		BlockState blockstate = state.cycle(OPEN);
+		level.setBlock(pos, blockstate, 2);
+		if (blockstate.getValue(FLUIDLOGGED)==MultiloggingEnum.WATER) {
+			level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+		}
+		if (blockstate.getValue(FLUIDLOGGED)==MultiloggingEnum.LAVA) {
+			level.scheduleTick(pos, Fluids.LAVA, Fluids.LAVA.getTickDelay(level));
+		}
+
+		this.playSound(player, level, pos, blockstate.getValue(OPEN));
 	}
 
 	@Override
