3987eb0227c2609dedf179e33b1284624db6f6a2
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2025-05-27 23:39:03 -0600

    fix thousand blade slash spread

--- src/main/java/com/bigdious/risus/items/weapons/ThousandBladeItem.java
+++ src/main/java/com/bigdious/risus/items/weapons/ThousandBladeItem.java
@@ -8,9 +8,9 @@ import com.bigdious.risus.init.RisusTags;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlotGroup;
@@ -74,66 +74,28 @@ public class ThousandBladeItem extends SwordItem {
 			int i = this.getUseDuration(stack, entity) - useTicks;
 			if (i >= 20) {
 				if (!level.isClientSide()) {
-					this.shoot((ServerLevel) level, player, player.getUsedItemHand(), stack);
-					if (!player.isCreative()) player.hurt(entity.damageSources().source(RisusDamageTypes.VAMPIRISM), 1 + 2 * stack.getEnchantmentLevel(level.registryAccess().holderOrThrow(Enchantments.MULTISHOT)));
+					int multishot = stack.getEnchantmentLevel(level.registryAccess().holderOrThrow(Enchantments.MULTISHOT));
+					this.shoot((ServerLevel) level, player, player.getUsedItemHand(), stack, multishot);
+					if (!player.isCreative()) player.hurt(entity.damageSources().source(RisusDamageTypes.VAMPIRISM), 1 + 2 * multishot);
 				}
 				level.playSound(null, entity, RisusSoundEvents.THOUSAND_BLADE_SLASH.get(), SoundSource.NEUTRAL, 1F, 0.6F);
 				player.awardStat(Stats.ITEM_USED.get(this));
 			}
-
-//			if (!level.isClientSide()) {
-//					stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(player.getUsedItemHand()));
-//					//TODO account for higher possible levels of multishot
-//					if (stack.getEnchantmentLevel(level.registryAccess().holderOrThrow(Enchantments.MULTISHOT)) > 0) {
-//						player.hurt(entity.damageSources().source(RisusDamageTypes.VAMPIRISM), 3);
-//
-//						BloodSlash slash1 = new BloodSlash(level, player, stack);
-//						BloodSlash slash2 = new BloodSlash(level, player, stack);
-//						slash.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.3F, 1.0F);
-//						slash1.shootFromRotation(player, player.getXRot(), player.getYRot() + 10, 0.0F, 1.3F, 1.0F);
-//						slash2.shootFromRotation(player, player.getXRot(), player.getYRot() - 10, 0.0F, 1.3F, 1.0F);
-//						level.addFreshEntity(slash);
-//						level.addFreshEntity(slash1);
-//						level.addFreshEntity(slash2);
-//						level.playSound(null, entity, RisusSoundEvents.THOUSAND_BLADE_SLASH.get(), SoundSource.NEUTRAL, 1F, 0.6F);
-//					} else {
-//						player.hurt(entity.damageSources().source(RisusDamageTypes.VAMPIRISM), 1);
-//						BloodSlash slash = new BloodSlash(level, player, stack);
-//						slash.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.3F, 1.0F);
-//						level.addFreshEntity(slash);
-//						level.playSound(null, entity, RisusSoundEvents.THOUSAND_BLADE_SLASH.get(), SoundSource.NEUTRAL, 1F, 0.6F);
-//					}
-//				}
-//				player.awardStat(Stats.ITEM_USED.get(this));
-//			}
 		}
 	}
-	private static Vector3f getProjectileShotVector(LivingEntity shooter, Vec3 distance, float angle) {
-		Vector3f vector3f = distance.toVector3f().normalize();
-		Vector3f vector3f1 = (new Vector3f(vector3f)).cross(new Vector3f(0.0F, 1.0F, 0.0F));
-		if ((double)vector3f1.lengthSquared() <= 1.0E-7) {
-			Vec3 vec3 = shooter.getUpVector(1.0F);
-			vector3f1 = (new Vector3f(vector3f)).cross(vec3.toVector3f());
-		}
 
-		Vector3f vector3f2 = (new Vector3f(vector3f)).rotateAxis(((float)Math.PI / 2F), vector3f1.x, vector3f1.y, vector3f1.z);
-		return (new Vector3f(vector3f)).rotateAxis(angle * ((float)Math.PI / 180F), vector3f2.x, vector3f2.y, vector3f2.z);
-	}
-
-	protected void shoot(ServerLevel level, LivingEntity living, InteractionHand hand, ItemStack stack) {
-
-		float arrowSize = 1+stack.getEnchantmentLevel(level.registryAccess().holderOrThrow(Enchantments.MULTISHOT))*2;
+	protected void shoot(ServerLevel level, LivingEntity living, InteractionHand hand, ItemStack stack, int multishot) {
+		float arrowSize = 1 + multishot * 2;
 		float f = EnchantmentHelper.processProjectileSpread(level, stack, living, 0.0F);
-		float f1 = arrowSize == 1 ? 0.0F : 2.0F * f / arrowSize-1;
-		float f2 = ((arrowSize-1) % 2) * f1 / 2.0F;
+		float f1 = arrowSize == 1 ? 0.0F : 2.0F * f / arrowSize - 1;
+		float f2 = ((arrowSize - 1) % 2) * f1 / 2.0F;
 		float f3 = 1.0F;
 
 		for (int i = 0; i < arrowSize; ++i) {
 			float f4 = f2 + f3 * (float) ((i + 1) / 2) * f1;
 			f3 = -f3;
 			BloodSlash slash = new BloodSlash(level, living, stack);
-			this.shootProjectile(living, slash, i, 1.3F, 1.0F, f4);
-			slash.shootFromRotation(living, living.getXRot(), living.getYRot()+f4, 0.0f, 1.3F, 1.0F);
+			this.shootProjectile(living, slash, 1.3F, 1.0F, f4);
 			level.addFreshEntity(slash);
 			stack.hurtAndBreak(1, living, LivingEntity.getSlotForHand(hand));
 			if (stack.isEmpty()) {
@@ -142,12 +104,12 @@ public class ThousandBladeItem extends SwordItem {
 		}
 	}
 
-	protected void shootProjectile(LivingEntity shooter, Projectile projectile, int index, float velocity, float inaccuracy, float angle) {
+	protected void shootProjectile(LivingEntity shooter, Projectile projectile, float velocity, float inaccuracy, float angle) {
 		Vector3f vector3f;
-			Vec3 vec3 = shooter.getUpVector(1.0F);
-			Quaternionf quaternionf = (new Quaternionf()).setAngleAxis((angle * ((float)Math.PI / 180F)), vec3.x, vec3.y, vec3.z);
-			Vec3 vec31 = shooter.getViewVector(1.0F);
-			vector3f = vec31.toVector3f().rotate(quaternionf);
+		Vec3 vec3 = shooter.getUpVector(1.0F);
+		Quaternionf quaternionf = (new Quaternionf()).setAngleAxis(angle * Mth.DEG_TO_RAD, vec3.x, vec3.y, vec3.z);
+		Vec3 vec31 = shooter.getViewVector(1.0F);
+		vector3f = vec31.toVector3f().rotate(quaternionf);
 
 		projectile.shoot(vector3f.x(), vector3f.y(), vector3f.z(), velocity, inaccuracy);
 	}
---
+++ src/main/java/com/bigdious/risus/items/weapons/package-info.java
@@ -0,0 +1,9 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+package com.bigdious.risus.items.weapons;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
