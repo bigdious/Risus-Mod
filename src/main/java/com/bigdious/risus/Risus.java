618402938f7b5a81940a5688dd1f29e1e54f5cd8
bigdious <bigdious@gmail.com>
2025-09-28 19:58:09 +0300

    fixed client packet issue

--- src/main/java/com/bigdious/risus/Risus.java
+++ src/main/java/com/bigdious/risus/Risus.java
@@ -12,6 +12,7 @@ import com.bigdious.risus.init.*;
 import com.bigdious.risus.init.RisusDataMaps;
 import com.bigdious.risus.network.CreateCritParticlePacket;
 import com.bigdious.risus.network.OpenBookPacket;
+import com.bigdious.risus.network.SyncCommonConfigPacket;
 import com.bigdious.risus.network.UnyieldingTotemPacket;
 import com.google.common.reflect.Reflection;
 import net.minecraft.core.Direction;
@@ -91,6 +92,7 @@ public class Risus {
 		registrar.playToClient(CreateCritParticlePacket.TYPE, CreateCritParticlePacket.STREAM_CODEC, CreateCritParticlePacket::handle);
 		registrar.playToClient(UnyieldingTotemPacket.TYPE, UnyieldingTotemPacket.STREAM_CODEC, UnyieldingTotemPacket::handle);
 		registrar.playToServer(OpenBookPacket.TYPE, OpenBookPacket.STREAM_CODEC,(payload, context) -> OpenBookPacket.handle(context));
+		registrar.playToClient(SyncCommonConfigPacket.TYPE, SyncCommonConfigPacket.STREAM_CODEC, SyncCommonConfigPacket::handle);
 	}
 
 	public void registerTypes(BlockEntityTypeAddBlocksEvent event) {
