f6102007aa4d37260247a56312a85cc9fbedc996
bigdious <bigdious@gmail.com>
2026-04-06 22:09:24 +0300

    final texture tweaks

--- src/main/java/com/bigdious/risus/data/ItemModelGenerator.java
+++ src/main/java/com/bigdious/risus/data/ItemModelGenerator.java
@@ -408,7 +408,7 @@ public class ItemModelGenerator extends ItemModelProvider {
 		singleTex(RisusItems.VEINS);
 		singleTex(RisusBlocks.BIG_CHAIN);
 		generatedRenderType(RisusBlocks.SPREADING_REMAINS.getId().getPath(), "minecraft:translucent", Risus.prefix("item/spreading_remains"));
-		generatedRenderType(RisusBlocks.ASHEN_SPIRE.getId().getPath(), "minecraft:cutout", Risus.prefix("block/ashen_spire_top"));
+		generatedRenderType(RisusBlocks.ASHEN_SPIRE.getId().getPath(), "minecraft:cutout", Risus.prefix("item/ashen_spire_item"));
 		singleTex(RisusBlocks.MEMORY1);
 		singleTex(RisusBlocks.REGEN_ROSE);
 		singleTex(RisusItems.LIGHT_DEVOURER);
